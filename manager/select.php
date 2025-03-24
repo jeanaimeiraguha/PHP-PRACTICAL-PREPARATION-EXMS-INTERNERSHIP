30f1777b3dd234228d4256b3b52b9b8191647ce8
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-24 19:39:35 +0200

    STOCK MAN

---
+++ manager/select.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+ <table>
+     <tr>
+          <th>ManagerID</th>
+          <th>User Name</th>
+          <th>Password</th>
+     </tr>
+     <?php
+     include 'conn.php';
+     $select=mysqli_query($conn, "SELECT* FROM manger ");
+     while($row=mysqli_fetch_array($select)){
+     ?>
+ </table>    
+</body>
+</html>

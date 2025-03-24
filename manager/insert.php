88ed752cfd731610c2e8044c9c1fdf1333b7cc01
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-24 19:31:44 +0200

    STOCK MAN

---
+++ manager/insert.php
@@ -0,0 +1,31 @@
+<?php
+include 'conn.php';
+if (isset($_POST['add'])) {
+     # code...
+     $UserName=$_POST['UserName'];
+     $Password=$_POST['Password'];
+     $insert=mysqli_query($conn,"INSERT INTO Manager VALUES('','$UserName','$Password')");
+if ($insert) {
+     # code...
+     echo "Inserted";
+}
+else{
+     echo "failed";
+}
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+  <form action="" method="post">
+     username<input type="text" name="UserName">
+     Password<input type="text" name="Password">
+  </form>   
+</body>
+</html>

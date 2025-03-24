2aa7e257b12069d6f86e6d995af9a5988677a13c
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-24 21:06:02 +0200

    STOCK MAN

--- funiture/delete.php
+++ funiture/delete.php
@@ -1,22 +1,3 @@
 <?php
-include 'conn.php';
 
-
-if (isset($_GET['FurnitureID'])) {
-    $FurnitureID = $_GET['FurnitureID'];
-
-  
-    $delete = mysqli_query($conn, "DELETE FROM furniture WHERE FurnitureID='$FurnitureID'");
-
-    if ($delete) {
-        echo "<script>
-                alert('Deleted successfully!');
-                window.location.href = 'select.php';
-              </script>";
-    } else {
-        echo "Failed to delete.";
-    }
-} else {
-    echo "FurnitureID is missing!";
-}
 ?>

416053b90ee78c4bf3fe6e165412fea96a5b0dc9
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-24 21:08:52 +0200

    STOCK MAN

--- funiture/delete.php
+++ funiture/delete.php
@@ -1,3 +1,13 @@
 <?php
-
+if (isset($_POST['FurnitureID'])) {
+     # code...
+}$FurnitureID=$_POST['FurnitureID'];
+$delete=mysqli_query($conn , "DELETE FROM funiture where FurnitureID='$FurnitureID'");
+if ($delete) {
+     # code...
+     echo "dlted";
+}
+else{
+     echo "failed";
+}
 ?>

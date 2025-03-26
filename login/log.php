82bf5a433004480e68abb439834ff476578f2bf0
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-26 16:19:46 +0200

    php

---
+++ login/log.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+  <form action="" method="post">
+     username<input type="text" name="UserName"> <br><br>
+     Password<input type="password" name="Password"> <br><br>
+<button name="add">Add New</button>  
+</form>   
+
+</body>
+</html>
+
+
+<?php
+
+
+?>
